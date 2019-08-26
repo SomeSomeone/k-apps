31a14206145443a7b6b3d39a91517333377560ae
gleb_orl <nice.gl_orl@ukr.net>
2019-08-27 01:18:42 +0300

    EventBus + animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,11 +3,13 @@
     <div class="grid" >
       <h2>Hello Gleb!</h2>
     </div>
-    <Goodlive :goodLive="goodLive"/>
+    <transition name="fade">
+      <Goodlive :goodLive="goodLive" v-if="isNeedShow"/>
+    </transition>
     <GoodliveList />
   </div>
 </template>
-<style>
+<style scoped>
   .home{
     padding: 32px 0;
   }
@@ -20,6 +22,13 @@
     color: var(--brand-color);
     grid-column: span 8;
   }
+  .fade-enter-active, .fade-leave-active {
+    transition: all .5s;
+  }
+  .fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+    opacity: 0;
+    transform: translateY(100px)
+  }
 </style>
 
 <script>
@@ -28,13 +37,26 @@ import GoodliveList from '@/components/goodlive/GoodliveList.vue'
 
 export default {
   name: 'home',
+  data: function () {
+    return {
+      isNeedShow: true
+    }
+  },
   components: {
     Goodlive,
     GoodliveList
   },
   props: {
     goodLive: String
+  },
+  watch: {
+    $route (to, from) {
+      var $this = this
+      this.isNeedShow = false
+      this.$root.$nextTick(function () {
+        $this.isNeedShow = true
+      })
+    }
   }
-
 }
 </script>
